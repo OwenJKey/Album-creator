88e2ef67c55e37e5385f1e0f26f3173b672ac80d
owen key <81066549+OwenJKey@users.noreply.github.com>
2024-03-04 20:50:07 -0700

    Add files via upload

--- assignment-1/js/index.js
+++ assignment-1/js/index.js
@@ -7,28 +7,21 @@
             form.addEventListener('submit', function(event) {
                 event.preventDefault();
 
-                if (titleInput.value.length > 20) {
+                if (titleInput.value.length > 20 || titleInput.value.length < 1) {
                     titleInput.classList.add('is-invalid');
                     return;
                 } else {
                     titleInput.classList.remove('is-invalid');
                 }
 
-                if (descriptionInput.value.length > 40 ) {
+                if (descriptionInput.value.length > 40 ||descriptionInput.value.length < 1 ) {
                     descriptionInput.classList.add('is-invalid');
                     return;
                 } else {
                     descriptionInput.classList.remove('is-invalid');
                 }
 
-                if (artInput.options === '0') {
-                    artInput.classList.add('is-invalid');
-                    return;
-                } else {
-                    artInput.classList.remove('is-invalid');
-                }
-
-                alert('Form submitted successfully!');
+                alert('Form submitted');
                 form.reset();
             });
           
