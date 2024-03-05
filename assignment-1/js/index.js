2400ee585f5db1b549ff298b178bb21a78982bed
owen key <81066549+OwenJKey@users.noreply.github.com>
2024-03-04 20:46:29 -0700

    Add files via upload

--- assignment-1/js/index.js
+++ assignment-1/js/index.js
@@ -1,36 +1,35 @@
+ document.addEventListener("DOMContentLoaded", function() {
+            const form = document.getElementById('album-form');
+            const titleInput = document.getElementById('album-title-input');
+            const descriptionInput = document.getElementById('album-description-input');
+            const artInput = document.getElementById('album-art-input');
+            
+            form.addEventListener('submit', function(event) {
+                event.preventDefault();
 
-const form = document.querySelector('form');
-const albumInput = document.querySelector('#album-title-input');
-const albumArtInput = document.querySelector('#albume-art-input');
-const albumInputDes = document.querySelector('#album-description-input');
-const errorStyle = 'text-red-500 text-xs';
-const errorDefaultStyle = 'text-xs text-zinc-700 mr-auto';
+                if (titleInput.value.length > 20) {
+                    titleInput.classList.add('is-invalid');
+                    return;
+                } else {
+                    titleInput.classList.remove('is-invalid');
+                }
 
-function isEmptySpace(str) {
-    return !str.trim().length;
-  }
-albumInput.addEventListener('input', (e) =>
- {
-    // Check if the input length is exactly 20 characters
-    if (albumInput.value.length >= 1 || albumInput.value.length <=10) {
-      console.log("Input is valid.");
-      return true;
-    } else {
-      console.log("Input is invalid.");
-      return false;
+                if (descriptionInput.value.length > 40 ) {
+                    descriptionInput.classList.add('is-invalid');
+                    return;
+                } else {
+                    descriptionInput.classList.remove('is-invalid');
+                }
 
-    }
+                if (artInput.options === '0') {
+                    artInput.classList.add('is-invalid');
+                    return;
+                } else {
+                    artInput.classList.remove('is-invalid');
+                }
 
-  });
-  
- albumInputDes.addEventListener('input', (e) =>{
-    if (albumInputDes.value.length >= 0 || albumInputDes.ariaValueMax.length <= 30) {
-        console.log("valid Description Name");
-        return true;
-      } else {
-        console.log("Invalid Description Name");
-        return false;
-  
-      }
- 
-  });
+                alert('Form submitted successfully!');
+                form.reset();
+            });
+          
+        });
